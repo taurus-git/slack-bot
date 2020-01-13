ede085fddb190ec5a718a062dcfa27208f713e21
taurus-git <t.1113@ya.ru>
2020-01-13 16:07:10 +0200

    Changed date format to timestamp. Removed unused var and method

--- src/components/SingleWorker.js
+++ src/components/SingleWorker.js
@@ -3,30 +3,21 @@ import React, { Component, PropTypes } from 'react';
 class SingleWorker extends Component {
     constructor(props) {
         super(props);
-
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick (username){
-        return function(){
-            //console.log(username);
-        };
-
     }
 
     render() {
         const { username, name, surname, congratulationMessage } = this.props;
 
         return (
-            /*<h3>{`${name} ${surname} ${congratulationMessage}`}</h3>*/
-            <li onClick={this.handleClick(username)}>
-                {`${name} ${surname} ${congratulationMessage}`}
+            <li>
+                {`${username} ${name} ${surname} ${congratulationMessage}`}
             </li>
         );
     }
 }
 
 SingleWorker.propTypes = {
+    username: PropTypes.string,
     name: PropTypes.string,
     surname: PropTypes.string,
     congratulationMessage: PropTypes.string,
